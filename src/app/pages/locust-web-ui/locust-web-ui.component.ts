308ac89e35e0af45f488e98416c69c8de443265c
Jack Slingerland <jack.slingerland@gmail.com>
2018-10-16 16:23:50 -0400

    View locust dashboard

--- src/app/pages/locust-web-ui/locust-web-ui.component.ts
+++ src/app/pages/locust-web-ui/locust-web-ui.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService, HttpRequestOptions } from '../../services/http.service';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-locust-web-ui',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./locust-web-ui.component.css']
 })
 export class LocustWebUiComponent implements OnInit {
+  swarm;
 
-  constructor() { }
+  constructor(private http: HttpService,
+              private route: ActivatedRoute,
+              private sanitizer: DomSanitizer) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const id = this.route.snapshot.params.id;
+    const options: HttpRequestOptions = {
+      authenticated: true,
+      requestType: 'GET',
+      url: `/api/v1/swarm/${id}`
+    };
+    const { data } = await this.http.request(options);
+    this.swarm = data;
   }
 
+  getSwarmAddress() {
+    console.log(`http://${this.swarm.master_ip}:8089`);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`http://${this.swarm.master_ip}:8089`);
+  }
 }
