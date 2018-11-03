6788043c6ab297417a52663377f8338b71f211ab
Jack Slingerland <jack.slingerland@gmail.com>
2018-11-03 09:50:49 -0400

    Deploy

--- src/deploy/deploy.js
+++ src/deploy/deploy.js
@@ -3,7 +3,7 @@ const shelljs = require("shelljs");
 (async () => {
     console.log("Starting client deploy....");
     const sourcePath = '/Users/jack/repos/roboswarm-client/dist';
-    const destinationPath = `/var/www/roboswarm-static`;
+    const destinationPath = `/var/www/roboswarm-static/static`;
     const command = `scp -r ${sourcePath} root@45.55.43.188:${destinationPath}`;
     console.log(command);
     shelljs.exec(`ssh root@45.55.43.188 "mkdir -p ${destinationPath}"`);
