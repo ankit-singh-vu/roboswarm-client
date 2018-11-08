21e9fc99225823a2f4e259f3e5a9a89f90fb277f
Jack Slingerland <jack.slingerland@gmail.com>
2018-11-08 13:02:29 -0500

    Fixing deploy script

--- src/deploy/deploy.js
+++ src/deploy/deploy.js
@@ -1,8 +1,13 @@
 const shelljs = require("shelljs");
+const fs = require('fs');
 
 (async () => {
     console.log("Starting client deploy....");
-    const sourcePath = '/Users/jack/repos/roboswarm-client/dist';
+    let sourcePath = '/Users/jack/repos/roboswarm-client/dist';
+    if (!fs.existsSync(sourcePath)) {
+        sourcePath = '/home/jack/repos/roboswarm/roboswarm-client/dist';
+    }
+    console.log("Using source path: ", sourcePath);
     const destinationPath = `/var/www/roboswarm-static/static`;
     const command = `scp -r ${sourcePath} root@45.55.43.188:${destinationPath}`;
     console.log(command);
