c6b1255170d07141ce69109066ae9bf734e01883
Jack Slingerland <jack.slingerland@gmail.com>
2019-05-20 12:21:41 -0400

    WIP - Deploy changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://roboswarm.us',
+  serverUrl: 'https://roboswarm.kernl.us',
   stripeApiPublic: 'sk_live_cb6FtYtqj4u0H650FPArHsbP'
 };
