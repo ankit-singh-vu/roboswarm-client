0cd787b1a2d0f7576721c691570b8a62d1d42906
Jack Slingerland <jack.slingerland@gmail.com>
2019-05-20 12:21:01 -0400

    WIP - Deploy changes

--- src/deploy/deploy.js
+++ src/deploy/deploy.js
@@ -9,9 +9,9 @@ const fs = require('fs');
     }
     console.log("Using source path: ", sourcePath);
     const destinationPath = `/var/www/roboswarm-static/static`;
-    const command = `scp -r ${sourcePath} root@45.55.43.188:${destinationPath}`;
+    const command = `scp -r ${sourcePath} root@roboswarm.kernl.us:${destinationPath}`;
     console.log(command);
-    shelljs.exec(`ssh root@45.55.43.188 "mkdir -p ${destinationPath}"`);
+    shelljs.exec(`ssh root@roboswarm.kernl.us "mkdir -p ${destinationPath}"`);
     shelljs.exec(command, () => {
         console.log("Client deploy complete.")
     });
