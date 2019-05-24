c663ad7efd1850c547de47f2aa6125540bbfdcf9
Jack Slingerland <jack.slingerland@gmail.com>
2019-05-24 08:14:37 -0400

    User interface for verifying sites completed.

--- src/app/pages/site-ownership-verification-add/site-ownership-verification-add.component.ts
+++ src/app/pages/site-ownership-verification-add/site-ownership-verification-add.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SiteOwnership, SiteOwnershipService } from '../../services/site-ownership.service';
+import { Router } from '@angular/router';
 
 interface SiteOwnershipUrl {
   url: string;
@@ -19,11 +20,17 @@ export class SiteOwnershipVerificationAddComponent {
   newSite: SiteOwnership;
   submitted = false;
 
-  constructor(private siteOwnershipService: SiteOwnershipService) { }
+  constructor(private siteOwnershipService: SiteOwnershipService,
+              private router: Router) { }
 
   async onSubmit() {
     this.submitted = true;
-    this.newSite = await this.siteOwnershipService.create({ base_url: this.model.url });
+    try {
+      this.newSite = await this.siteOwnershipService.create({ base_url: this.model.url });
+      this.router.navigate(['/site-ownership-verification']);
+    } catch (err) {
+      this.error = 'There was an error verifying your site. Try again.';
+    }
     this.submitted = false;
   }
 
