0c8da69ee17c4defecc978cf540747f2598340e6
Jack Slingerland <jack.slingerland@gmail.com>
2020-06-11 12:52:47 -0400

    WIP - Adding routes

--- src/app/pages/templates-add-edit/templates-add-edit.component.ts
+++ src/app/pages/templates-add-edit/templates-add-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Template, TemplateRoute } from '../../services/template.service';
+import { NgForm } from '@angular/forms';
+import { SiteOwnershipService, SiteOwnership } from '../../services/site-ownership.service';
 
 interface AddEditTemplate extends Template {
   routes: TemplateRoute[];
@@ -15,12 +17,35 @@ export class TemplatesAddEditComponent implements OnInit {
     name: '',
     routes: []
   };
+  tmpRoute: '';
+  showRoutes: false;
+  sites: SiteOwnership[] = [];
   submitted = false;
+  working = true;
   error = '';
 
-  constructor() { }
+  constructor(private siteOwnershipService: SiteOwnershipService) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.working = true;
+    this.sites = await this.siteOwnershipService.getAll();
+    this.working = false;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(`Template name set: ${this.model.name}`);
+  }
+
+  addTmpRoute() {
+    this.model.routes.push({
+      method: 'GET',
+      path: this.tmpRoute
+    });
+    this.tmpRoute = '';
+  }
+
+  allFieldsCompleted(): boolean {
+    return this.model.name && this.model.name.trim() !== '';
   }
 
 }
