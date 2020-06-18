de6eed869512c6e3ac889e727b3122b8b3349ec9
Jack Slingerland <jack.slingerland@gmail.com>
2020-06-18 09:16:26 -0400

    Removing amplitude. Fixing started_at

--- src/app/services/metrics.service.ts
+++ src/app/services/metrics.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
-declare const amplitude: any;
-
 @Injectable()
 export class MetricsService {
 
@@ -10,15 +8,16 @@ export class MetricsService {
 
   track(event: string, data?: Object): void {
     if (environment.production) {
-      try {
-        if (!data) {
-          amplitude.getInstance().logEvent(event);
-        } else {
-          amplitude.getInstance().logEvent(event, data);
-        }
-      } catch (err) {
-        console.log('Error tracking event: ', err);
-      }
+      console.log({ event, data });
+      // try {
+      //   if (!data) {
+      //     amplitude.getInstance().logEvent(event);
+      //   } else {
+      //     amplitude.getInstance().logEvent(event, data);
+      //   }
+      // } catch (err) {
+      //   console.log('Error tracking event: ', err);
+      // }
     } else {
       console.log('Metrics Development Fake Tracking: ', { event, data });
     }
