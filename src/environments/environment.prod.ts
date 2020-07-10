a08946d4b76e78f58381f59d66323fc1bc52da09
Jack Slingerland <jack.slingerland@gmail.com>
2020-07-10 09:26:55 -0400

    Fixing asset base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   serverUrl: 'https://roboswarm.kernl.us',
   stripeApiPublic: 'pk_live_g8FjokEYtLkaHf9AqFz4C5hP',
-  assetBase: '/static/dist/'
+  assetBase: '/'
 };
