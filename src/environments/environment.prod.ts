500de07c45555ad705e1c2992d7a1c6f3093aba3
Jack Slingerland <jack.slingerland@gmail.com>
2020-07-09 20:37:29 -0400

    environment tweaks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-import { AngularEnvironment } from "./environment";
-
-export const environment: AngularEnvironment = {
+export const environment = {
   production: true,
   serverUrl: 'https://roboswarm.kernl.us',
   stripeApiPublic: 'pk_live_g8FjokEYtLkaHf9AqFz4C5hP',
