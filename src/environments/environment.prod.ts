f626a6b3f8e51be84c6d86a5c343b216a67ef64f
Jack Slingerland <jack.slingerland@gmail.com>
2021-04-05 20:46:04 -0400

    Fixing env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: process.env.SERVER_URL || 'https://roboswarm.dev',
+  serverUrl: 'https://roboswarm.dev',
   stripeApiPublic: 'pk_live_g8FjokEYtLkaHf9AqFz4C5hP'
 };
