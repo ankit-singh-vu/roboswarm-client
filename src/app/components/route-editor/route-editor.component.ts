e95bfec9ea6a5763a22b338a5ab00e88bc45e0af
Jack Slingerland <jack.slingerland@gmail.com>
2021-06-04 07:25:50 -0400

    WIP - Route editor

--- src/app/components/route-editor/route-editor.component.ts
+++ src/app/components/route-editor/route-editor.component.ts
@@ -13,9 +13,13 @@ export class RouteEditorComponent implements OnInit {
   @Input() onChange: any;
 
   throttledOnChange: DebouncedFunc<any>;
-
+  activeTab = 1;
   method: RouteMethod;
   path: string;
+  headers: string = '';
+  queryParams: string = '';
+  body: string = '';
+  requestBody: string = '';
   methods = [
     { value: 'GET', name: 'GET' },
     { value: 'POST', name: 'POST' },
@@ -28,18 +32,27 @@ export class RouteEditorComponent implements OnInit {
 
   ngOnInit() {
     this.throttledOnChange = throttle(this.onChange, 500, { trailing: true });
+    // TODO when component init break apart the advancedTemplateROute into
+    // parts and set all the variables so form changes pick up.
+  }
+
+  getKeyValueFromString(data: string): { key: string; value: string; }[] {
+    return data
+      .split(/\r?\n/)
+      .filter(d => d.split(':').length === 2)
+      .map(d => {
+        const arr = d.split(':');
+        return { key: arr[0], value: arr[1] };
+      });
   }
 
   onDataChange() {
     this.throttledOnChange({
       ...this.route,
       path: this.path,
-      method: this.method
+      method: this.method,
+      headers: this.getKeyValueFromString(this.headers),
+      queryParams: this.getKeyValueFromString(this.queryParams)
     });
   }
-
-  getRouteMethods() {
-    return Object.values(RouteMethod);
-  }
-
 }
