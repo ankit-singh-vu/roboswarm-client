08509a19ed4081d0c6c7491e35a3cddd732183e8
Jack Slingerland <jack.slingerland@gmail.com>
2023-04-27 13:33:52 -0400

    Updating ENV

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://elite.roboswarm.dev',
+  serverUrl: 'https://roboswarm.dev',
   stripeApiPublic: 'pk_live_g8FjokEYtLkaHf9AqFz4C5hP'
 };
