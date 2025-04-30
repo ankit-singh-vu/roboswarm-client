23e1369026db3ffb7af6644f1d1f60ac663690f5
Ankit Singh <76851815+ankit-singh-vu@users.noreply.github.com>
2025-04-30 16:58:56 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,11 @@ interface AngularEnvironment {
 
 export const environment: AngularEnvironment = {
   production: false,
-  serverUrl: 'http://localhost:3002',
-  stripeApiPublic: '' // Stripe public key. Used for adding/editing payment details
+  // serverUrl: 'http://localhost:3002',
+  // serverUrl: 'http://localhost:3000',
+  // serverUrl: 'http://213.165.232.14:3000',
+  // serverUrl: 'http://roboswarm.convesio.e247.site:3000',
+  // serverUrl: 'https://roboswarm.convesio.e247.site/api',
+  serverUrl: 'https://roboswarm.convesio.do.e247.site/backend',
+  stripeApiPublic: 'pk_test_51Ig7WwSACg5J6CChdECBkzMhxopcY18EQyEGcaWakip7FuKjYyYxQUZCRFcTN2OD2mbDOc8lnnFQ7iId9tXesKyl00U8J3wxMg' // Stripe public key. Used for adding/editing payment details
 };
